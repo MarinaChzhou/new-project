044479d3ae2f98af345fb9ff8512a04de5af0d3b
Marina Chzhou <uspeh.mc@gmail.com>
2023-02-24 12:44:45 +1000

    addition

---
+++ lesson.py
@@ -0,0 +1,19 @@
+def gcd(a, b):
+
+    while a != 0 and b != 0:
+
+        if a > b:
+
+            a = a % b
+            a_1 = a
+            b_1 = b
+            ab = a_1 + b_1
+        else:
+
+            b = b % a
+            a_1 = a
+            b_1 = b
+            ab = a_1 + b_1
+    print('Наибольший общий делитель:', a + b)
+
+
